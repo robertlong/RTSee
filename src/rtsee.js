850dad6aa34e0232d899658e491e8887dcd971ff
Robert <robert@robertlong.me>
2014-09-18 19:48:45 -0700

    Commented code and increased code consistency. Removed lodash dependency.

--- src/rtsee.js
+++ src/rtsee.js
@@ -1,51 +1,69 @@
-var _ = require('lodash');
+var extend = require('extend');
 
+// Alias for vendor getUserMedia API
 navigator.getUserMedia = navigator.getUserMedia ||
                          navigator.webkitGetUserMedia ||
                          navigator.mozGetUserMedia ||
-                         navigator.msGetUserMedia; 
+                         navigator.msGetUserMedia;
 
-var AudioContext = window.webkitAudioContext || window.AudioContext;
+// Keeps the canvas from rendering at unessessary speeds
+var FRAMERATE_CAP = 30;// fps
 
 function RTSee (canvasEl, opts) {
-  this.canvasEl = canvasEl;
-  this.videoEl = document.createElement('video');
-  this.backEl = document.createElement('canvas');
-
-  this.canvasCtx = canvasEl.getContext('2d');
-  this.backCtx = this.backEl.getContext('2d');
-
-  this.opts = _.extend({
+  this.opts = extend({
     video: true,
     audio: true,
     width: 640,
     height: 480,
     micPlaybackMuted: false,
-    filter: 'default'
+    filter: 'default',
+    onError: function () {}
   }, opts);
+
+  this.canvasEl = canvasEl;
+  this.videoEl = document.createElement('video');
+  this.backEl = document.createElement('canvas');
+
+  this.backEl.width = this.canvasEl.width = this.opts.width;
+  this.backEl.height = this.canvasEl.height = this.opts.height;
+
+  this.canvasCtx = canvasEl.getContext('2d');
+  this.backCtx = this.backEl.getContext('2d');
 
   this.videoEl.muted = this.opts.micPlaybackMuted;
 
-  navigator.getUserMedia({video: this.opts.video, audio: this.opts.audio}, 
+  navigator.getUserMedia(
+    {video: this.opts.video, audio: this.opts.audio}, 
     this._onMediaSuccess.bind(this), 
     this._onMediaError.bind(this));
 }
 
-_.extend(RTSee.prototype, {
+extend(RTSee.prototype, {
   
+  /**
+   * Use the mediaStream returned from getUserMedia to render out to the videoEl.
+   * Add an event listener to start rendering the canvas when 
+   */
   _onMediaSuccess: function (mediaStream) {
-    this.stream = mediaStream;
-    
-    this.videoEl.src = window.URL.createObjectURL(this.stream);
+    this.videoEl.src = window.URL.createObjectURL(mediaStream);
     this.videoEl.play();
 
     this.videoEl.addEventListener('playing', this._onPlaying.bind(this), false);
   },
 
+  /**
+   * If there was an error getting the user's mediastream. Log an error
+   * and call the provided onError function.
+   */
   _onMediaError: function (err) {
     console.error(err);
+    this.opts.onError(err);
   },
 
+  /**
+   * Once the video has begun playing, render the video to the canvas at
+   * FRAMERATE_CAP fps.
+   */
   _onPlaying: function () {
     var video = this.videoEl;
     var canvas = this.canvasEl;
@@ -56,40 +74,45 @@ _.extend(RTSee.prototype, {
     var ch = this.opts.height;
     var renderCanvas = this._renderCanvas.bind(this);
 
-    canvas.width = cw;
-    canvas.height = ch;
-    back.width = cw;
-    back.height = ch;
-
-    var fps = 30;
     var draw = function () {
         setTimeout(function() {
             requestAnimationFrame(draw);
             renderCanvas(video, ctx, backCtx, cw, ch, this.opts.filter); 
-        }.bind(this), 1000 / fps);
+        }.bind(this), 1000 / FRAMERATE_CAP);
     }.bind(this);
 
     draw();
   },
 
+  /**
+   * Render the video stream to the canvas.
+   * Apply any applicable filters.
+   */ 
   _renderCanvas: function (v, c, bc, w, h, filter) {
     if(v.paused || v.ended) return false;
 
-    // First, draw it into the backing canvas
-    bc.drawImage(v,0,0,w,h);
+    // If there is no filter applied, draw the video directly to the canvas.
+    if (filter === 'default') {
+      c.drawImage(v, 0, 0, w, h);
+      return;
+    }
+
+    // Draw the video into the backing canvas
+    bc.drawImage(v, 0, 0, w, h);
     // Grab the pixel data from the backing canvas
     var idata = bc.getImageData(0,0,w,h);
     var data = idata.data;
 
+    // Apply the filter to the pixel data
     switch (filter) {
       case 'greyscale':
         this._filterGreyscale(data);
         break;
       case 'sepia':
-        this._filterSepia(data, w, h);
+        this._filterSepia(data);
         break;
       case 'invert':
-        this._filterInvert(data, w, h);
+        this._filterInvert(data);
         break;
     }
 
@@ -99,63 +122,60 @@ _.extend(RTSee.prototype, {
     c.putImageData(idata, 0, 0);
   },
 
+  /**
+   * Image data format:
+   * data = [red(0,0), green(0,0), blue(0,0), alpha(0,0)]
+   */
+
+  // Make the image data greyscale.
   _filterGreyscale: function (data) {
-    // Loop through the pixels, turning them grayscale
     for(var i = 0; i < data.length; i+=4) {
       var r = data[i];
       var g = data[i+1];
       var b = data[i+2];
+      
       var brightness = (3*r+4*g+b)>>>3;
+
       data[i] = brightness;
       data[i+1] = brightness;
       data[i+2] = brightness;
     }
   },
 
-  _filterSepia: function (data, w, h) {
-    var w4 = w * 4;
-    var y = h;
-    do {
-      var offsetY = (y - 1) * w4;
-      var x = w;
-      do {
-        var offset = offsetY + (x - 1) * 4;
+  // Make the image data more orange.
+  _filterSepia: function (data) {
+    for(var i = 0; i < data.length; i+=4) {
+      var or = data[i];
+      var og = data[i + 1];
+      var ob = data[i + 2];
 
-        var or = data[offset];
-        var og = data[offset + 1];
-        var ob = data[offset + 2];
+      var r = (or * 0.393 + og * 0.769 + ob * 0.189);
+      var g = (or * 0.349 + og * 0.686 + ob * 0.168);
+      var b = (or * 0.272 + og * 0.534 + ob * 0.131);
 
-        var r = (or * 0.393 + og * 0.769 + ob * 0.189);
-        var g = (or * 0.349 + og * 0.686 + ob * 0.168);
-        var b = (or * 0.272 + og * 0.534 + ob * 0.131);
+      if (r < 0) r = 0;
+      if (r > 255) r = 255;
+      if (g < 0) g = 0;
+      if (g > 255) g = 255;
+      if (b < 0) b = 0;
+      if (b > 255) b = 255;
 
-        if (r < 0) r = 0;
-        if (r > 255) r = 255;
-        if (g < 0) g = 0;
-        if (g > 255) g = 255;
-        if (b < 0) b = 0;
-        if (b > 255) b = 255;
-
-        data[offset] = r;
-        data[offset + 1] = g;
-        data[offset + 2] = b;
-
-      } while (--x);
-    } while (--y);
-  },
-
-  _filterInvert: function (data, w, h) {
-    for (var y = 1; y < h - 1; y++) {
-      for (var x = 1; x < w - 1; x++) {
-        var pixel = (y * w + x) * 4;
-        data[pixel] = Math.round(255 - data[pixel]);
-        data[pixel + 1] = Math.round(255 - data[pixel + 1]);
-        data[pixel + 2] = Math.round(255 - data[pixel + 2]);
-        data[pixel + 3] = data[pixel + 3];
-      }   
+      data[i] = r;
+      data[i + 1] = g;
+      data[i + 2] = b;  
     }
   },
 
+  // Invert the colors in the image data.
+  _filterInvert: function (data) {
+    for(var i = 0; i < data.length; i += 4) {
+      data[i] = Math.round(255 - data[i]); // Red
+      data[i + 1] = Math.round(255 - data[i + 1]); // Green
+      data[i + 2] = Math.round(255 - data[i + 2]); // Blue
+    }
+  },
+
+  // Returns the image captured from the provided canvas element.
   captureScreenshot: function () {
     var canvas = this.canvasEl;
     var dataURL = canvas.toDataURL('image/png');
@@ -166,10 +186,14 @@ _.extend(RTSee.prototype, {
     return img;
   },
 
+  /**
+   * Mutes and unmutes the playback of the microphone.
+   * Note: This doesn't keep the microphone from streaming.
+   */
   toggleMicPlayback: function () {
     this.videoEl.muted = !this.videoEl.muted;
   }
-  
+
 });
 
 module.exports = RTSee;
